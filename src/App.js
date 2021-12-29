6dcb76fdbda79133a54dcc4632119eb317ac7bb3
Victoria <viktoriyaivanyutenko@yandex.ru>
2021-12-29 18:03:33 +0300

    Сделаны карточки пицц

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { Header } from "./components";
 import { Home, Cart } from "./pages";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/db.json")
+      .then((response) => response.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<Home items={pizzas} />} exact />
           <Route path="/Cart" element={<Cart />} exact />
         </Routes>
       </div>
