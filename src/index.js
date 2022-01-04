5df3f12045e37f65c68ee7698fb214507093094f
Victoria <viktoriyaivanyutenko@yandex.ru>
2022-01-04 15:30:47 +0300

    Started Redux trip

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 import "./scss/app.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
